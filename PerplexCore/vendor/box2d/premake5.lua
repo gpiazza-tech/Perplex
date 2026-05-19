b9cf448f440dfa7dde03e857b11fb7fdb3e70b74
Gabriel Piazza <contact@gpiazza.me>
2026-05-18 22:32:48 -0400

    Added box2d as vendor

---
+++ PerplexCore/vendor/box2d/premake5.lua
@@ -0,0 +1,30 @@
+project "box2d"
+	kind "StaticLib"
+	language "C"
+    staticruntime "on"
+	toolset "msc-v145"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/**.h",
+        "src/**.c",
+	}
+
+	includedirs
+	{
+		"include",
+	}
+
+	filter "system:windows"
+        systemversion "latest"
+		
+	filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
